94abd4c0a8cbd7eb70f735c40b92e93142ef3c62
Rafi Rahman <rafirahmann18@gmail.com>
2022-01-18 16:43:27 +0700

    yahahahayyuk

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,7 +1,13 @@
 <?php
-    class About{
+    class About extends Controller{
+        public function index($name = "Rafi",$address = "Malang",$age = 16){
+            $data['name'] = $name;
+            $data['address'] = $address;
+            $data['age'] = $age;
+            $this->view("about/index", $data);
+        }
         public function page(){
-            echo 'About/page';
+            $this->view("about/page");
         }
     }
 ?>
--- app/core/App.php
+++ app/core/App.php
@@ -1,5 +1,6 @@
 <?php
     class App{
+
         protected $controller = 'home'; // file
         protected $method = 'index'; // function
         protected $params = [];
@@ -26,14 +27,14 @@
             // Params
             if(!empty($url)){
                 $this->params = array_values($url);
-                var_dump($url);
             }
 
             // Jalankan controller & method, serta kirimkan params jika ada
             call_user_func_array([$this->controller, $this->method], $this->params);
         }
 
-        public function parseURL(){            if(isset($_GET['url'])){
+        public function parseURL(){            
+            if(isset($_GET['url'])){
                 $url = rtrim($_GET['url'], '/'); // rtrim() digunakan untuk menghilangkan tanda '/' di akhir url
                 $url = filter_var($url, FILTER_SANITIZE_URL); // digunakan untuk mencegah request yang berbaya
                 $url = explode('/',$url);// digunkan untuk mengubah url menjadi string
